e22f0f97707c1a0f2dd04aa37525a35c008915b1
Seba <sequra128@gmail.com>
2025-09-29 00:33:54 -0300

    Merge remote-tracking branch 'origin/fix/planet_position_and_ar' into fix/planet_position_and_ar

--- app/main.tsx
+++ app/main.tsx
@@ -10,9 +10,10 @@ const Main = () => {
             webxr="requiredFeatures: hit-test,local-floor; optionalFeatures: dom-overlay,unbounded; overlayElement: #overlay;"
 
             xrweb="mode: immersive-ar; requiredFeatures: hit-test; optionalFeatures: dom-overlay; overlayElement: #overlay"
-            xr-mode-ui="enabled: true; enterAREnabled: true; XRMode: ar;"
+            xr-mode-ui="enabled: true; enterAREnabled: true; enterARButton: #myEnterARButton;  XRMode: xr;"
             renderer="alpha: true; colorManagement: true; antialias: true; foveationLevel: 1; physicallyCorrectLights: true"
         >
+            <a id="myEnterARButton" href="#">TOCAME AR PLS</a>
             <a-entity camera look-controls wasd-controls="acceleration:250; fly:true" position="0 10 150">
             </a-entity>
             <SolarSystem/>
